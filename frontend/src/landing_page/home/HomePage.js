df1eeb0ab1d2b852fa8709ea0b4392f6ce2c31c0
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2025-09-29 22:26:18 +0530

    update

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Hero from './Hero';
 import Stats from './Stats';
 import Navbar from '../Navbar';
+import Footer from '../Footer';
 
 function HomePage() {
     return ( 
